a5a7aa59420aff24f534cb704151fb5cce7c7fde
Jamie Diamond <jamiediamond@me.com>
2016-12-23 16:05:23 +0000

    inital

--- sensehat.py
+++ sensehat.py
@@ -1,9 +1,14 @@
-from sense_hat import SenseHat
+from sense_emu import SenseHat
+import matplotlib.pyplot as plt
 
 sense = SenseHat()
 
 sense.show_message("Hello my name is JD!!")
 
+
+p = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+r = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+y = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 
 while True:
@@ -11,5 +16,13 @@ while True:
     pitch = orientation['pitch']
     roll = orientation['roll']
     yaw = orientation['yaw']
-    print("pitch={0}, roll={1}, yaw={}\n".format(pitch,yaw,roll))
-
+    p.remove(0)
+    p.append(pitch)
+    r.remove(0)
+    r.append(roll)
+    y.remove(0)
+    y.append(yaw)
+    plt.plot(p)
+    plt.plot(r)
+    plt.plot(y)
+    plt.show()
