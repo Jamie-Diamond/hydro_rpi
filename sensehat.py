bc46546de89ebe4520a359b5d75b167f8b3a940b
JD@pi <jamiediamond@me.com>
2016-12-23 16:26:13 +0000

    update tested over ssh

--- sensehat.py
+++ sensehat.py
@@ -2,29 +2,30 @@ from sense_emu import SenseHat
 import matplotlib.pyplot as plt
 sense = SenseHat()
 
-sense.show_message("Hello my name is JD!!")
+sense.show_message("He")
 
 
 p = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 r = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 y = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-fig1 = plt.figure()
+plt.figure(1)
 while True:
     orientation = sense.get_orientation()
     pitch = orientation['pitch']
     roll = orientation['roll']
     yaw = orientation['yaw']
-    p.remove(0)
+    p.pop(0)
     p.append(pitch)
-    r.remove(0)
+    r.pop(0)
     r.append(roll)
-    y.remove(0)
+    y.pop(0)
     y.append(yaw)
-    plt.clf(fig1)
+    plt.cla()
     plt.plot(p)
     plt.plot(r)
     plt.plot(y)
+    plt.pause(0.000000000001)
 
 
 
